35fb882abdbe2a9a50320904e49a35da5e7472df
emanuelmgww <emanuelmirandagaldino40197@gmail.com>
2026-05-11 18:57:45 -0300

    fix: corrige funcionamento dos botões de fechar modal

--- src/assets/js/painelAdmin/modalExcluirReserva.js
+++ src/assets/js/painelAdmin/modalExcluirReserva.js
@@ -1,28 +1,17 @@
 const modalConfirmacao = document.getElementById("modal-confirmacao")
 
-document.querySelectorAll(".open-modal").forEach(btn => {
+document.querySelectorAll("[data-modal]").forEach(btn => {
     btn.addEventListener("click", () => {
-        const modalId = btn.dataset.modal
-        document.getElementById(modalId).showModal()
+        const modal = document.getElementById(btn.dataset.modal)
+        if (modal.open) {
+            modal.close()
+        } else {
+            modal.showModal()
+        }
     })
-})
-
-document.getElementById("btn-fechar-modal").addEventListener("click", () => {
-    modalConfirmacao.close()
 })
 
 document.getElementById("btn-confirmar-exclusao").addEventListener("click", () => {
-    /* Adicionar lógica para apgar oa reserva */
+    /* Adicionar lógica para apagar a reserva */
     modalConfirmacao.close()
-})
-
-document.getElementById("modal-confirmacao-btn-cancelar").addEventListener("click", () => {
-    modalConfirmacao.close()
-})
-
-document.querySelectorAll(".close-modal").forEach(btn => {
-    btn.addEventListener("click", () => {
-        const modalId = btn.dataset.modal
-        document.getElementById(modalId).close()
-    })
 })
