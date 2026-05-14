c5db64e50be7f5a10cb362d3334690cbae0913b2
Letícia Sousa Moreira <lele.moreira0405@gmail.com>
2026-05-14 13:34:07 -0300

    Ajuste notificações mobile e responsividade

--- src/assets/js/notificacoes.js
+++ src/assets/js/notificacoes.js
@@ -4,3 +4,9 @@ const notificationMenu = document.querySelector(".notification-menu");
 notificationBtn.addEventListener("click", () => {
     notificationMenu.classList.toggle("active");
 });
+
+document.addEventListener("click", (e) => {
+    if (!notificationMenu.contains(e.target) && !notificationBtn.contains(e.target)) {
+        notificationMenu.classList.remove("active");
+    }
+});
